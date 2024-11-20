630db4fedbb11d40ff232ad327c7a87bd757623f
KillianDlds <130392622+KillianDlds@users.noreply.github.com>
2024-11-20 14:35:55 +0100

    Update Q5.java

--- Q5.java
+++ Q5.java
@@ -1,10 +1,11 @@
 public class Q5 {
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]); 
+        int n = Integer.parseInt(args[0]);
 
-        for (int i = 1; i <= n; i++) {
+        // Afficher un triangle inversé avec des "x"
+        for (int i = n; i >= 1; i--) { 
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print("x");  // Remplacer "*" par x"
             }
             System.out.println();
         }
